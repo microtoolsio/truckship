f6574d339b6cedbcc05dc3ada89dfca3d724cc5a
bychkovav <aliaksei.bychkou@gmail.com>
2018-02-12 17:11:05 +0300

    auth

--- gateway/gateway/Startup.cs
+++ gateway/gateway/Startup.cs
@@ -58,9 +58,13 @@ namespace Gateway
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
-
 
             app.UseAuthentication();
 
